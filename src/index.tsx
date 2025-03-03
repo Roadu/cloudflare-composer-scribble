4b779892b503052cf97d1f41120da96799c92d14
Roadu <roadu_202@proton.me>
2025-03-03 09:41:59 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -42,14 +42,5 @@ app
   .hono.use("/draw", serveStatic({root: './public'}));
 
 
-  const isCloudflareWorker = typeof caches !== 'undefined'
-if (isCloudflareWorker) {
-  const manifest = await import('__STATIC_CONTENT_MANIFEST')
-  const serveStaticOptions = { manifest, root: './' }
-  app.use('/*', serveStatic(serveStaticOptions))
-  devtools(app, { assetsPath: '/frog', serveStatic, serveStaticOptions })
-} else {
-  devtools(app, { serveStatic })
-}
 
 export default app;
